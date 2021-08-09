f6d69878895b81cd666cd1313cbd9b2d1906050d
Omer Pariente <omerpr23@gmail.com>
2021-08-09 21:51:35 +0300

    port

--- client/src/components/Ip.vue
+++ client/src/components/Ip.vue
@@ -18,7 +18,7 @@ export default {
   },
   methods: {
     async getIpAndLocation() {
-      const path = 'http://localhost:5000/api/ip';
+      const path = 'http://localhost:8000/api/ip';
       try {
         const res = await axios.get(path);
         this.ip = res.data.ip;
